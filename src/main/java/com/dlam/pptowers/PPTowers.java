7f82ee109e81c4a413b2b6abac09a93a7799f8d4
lamvd <lamvdavid@gmail.com>
2020-11-30 17:12:44 -0500

    Initial Commit: Basic Fabric Mod Project Layout

---
+++ src/main/java/com/dlam/pptowers/PPTowers.java
@@ -0,0 +1,12 @@
+package com.dlam.pptowers;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PPTowers implements ModInitializer {
+    public static final String MOD_ID = "pptowers";
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
