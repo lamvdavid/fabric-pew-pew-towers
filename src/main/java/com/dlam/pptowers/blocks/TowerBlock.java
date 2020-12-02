d44fad0ce2091e13990e238f308d4f713691e009
lamvd <lamvdavid@gmail.com>
2020-12-02 14:14:55 -0500

    Create TowerBlock parent class

---
+++ src/main/java/com/dlam/pptowers/blocks/TowerBlock.java
@@ -0,0 +1,9 @@
+package com.dlam.pptowers.blocks;
+
+import net.minecraft.block.Block;
+
+public class TowerBlock extends Block {
+    public TowerBlock(Settings settings) {
+        super(settings);
+    }
+}
