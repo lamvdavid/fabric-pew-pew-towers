68937892a875cd54688c92db9e931db5b2b92910
lamvd <lamvdavid@gmail.com>
2020-12-07 17:15:29 -0500

    Move createProjectile logic to ArrowTower

--- src/main/java/com/dlam/pptowers/entities/ArrowTowerBlockEntity.java
+++ src/main/java/com/dlam/pptowers/entities/ArrowTowerBlockEntity.java
@@ -1,6 +1,10 @@
 package com.dlam.pptowers.entities;
 
 import com.dlam.pptowers.registry.BlockEntities;
+import net.minecraft.entity.projectile.ArrowEntity;
+import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.util.math.Position;
+import net.minecraft.world.World;
 
 public class ArrowTowerBlockEntity extends TowerBlockEntity{
     //Hard coded range. May change later if doing tower upgrades
@@ -11,5 +15,10 @@ public class ArrowTowerBlockEntity extends TowerBlockEntity{
 
     public ArrowTowerBlockEntity() {
         super(BlockEntities.ARROW_TOWER_BLOCK_ENTITY, X_RANGE, Y_RANGE, Z_RANGE, FIRE_RATE);
+    }
+
+    protected ProjectileEntity createProjectile(World world, Position position) {
+        ArrowEntity arrow = new ArrowEntity(world, (position.getX() + xFace), (position.getY() + yFace), (position.getZ() + zFace));
+        return arrow;
     }
 }
