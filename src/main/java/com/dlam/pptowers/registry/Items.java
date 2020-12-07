ceaa140f6f18447e36e0a53bdd206dc8f226349e
lamvd <lamvdavid@gmail.com>
2020-12-06 21:17:33 -0500

    Add ability to place TowerBlocks

--- src/main/java/com/dlam/pptowers/registry/Items.java
+++ src/main/java/com/dlam/pptowers/registry/Items.java
@@ -15,6 +15,7 @@ public class Items {
     public static final ItemGroup TOWER_GROUP = FabricItemGroupBuilder.build(
             new Identifier("pptowers", "general"),
             () -> new ItemStack(Items.ARROW_TOWER_ITEM));
+
     public static final ArrowTowerItem ARROW_TOWER_ITEM = new ArrowTowerItem(new FabricItemSettings().group(TOWER_GROUP));
 
     public static void registerItems() {
