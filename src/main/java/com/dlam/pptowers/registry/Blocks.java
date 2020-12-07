0328c2c163143365e17d56092f9d3f114c9ed4f5
lamvd <lamvdavid@gmail.com>
2020-12-06 21:09:51 -0500

    Change to use MOD_ID string instead of hard coding

--- src/main/java/com/dlam/pptowers/registry/Blocks.java
+++ src/main/java/com/dlam/pptowers/registry/Blocks.java
@@ -1,5 +1,6 @@
 package com.dlam.pptowers.registry;
 
+import com.dlam.pptowers.PPTowers;
 import com.dlam.pptowers.blocks.ArrowTowerBlock;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Material;
@@ -10,6 +11,6 @@ public class Blocks {
     public static final ArrowTowerBlock ARROW_TOWER_BLOCK = new ArrowTowerBlock(FabricBlockSettings.of(Material.STONE));
 
     public static void registerBlocks() {
-        Registry.register(Registry.BLOCK, new Identifier("pptowers", "arrow_tower_block"), ARROW_TOWER_BLOCK);
+        Registry.register(Registry.BLOCK, new Identifier(PPTowers.MOD_ID, "arrow_tower_block"), ARROW_TOWER_BLOCK);
     }
 }
