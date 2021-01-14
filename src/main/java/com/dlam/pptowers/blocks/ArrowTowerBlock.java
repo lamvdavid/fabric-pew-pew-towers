72202cd26b7bb686f29f92688d5e294db3130b7d
lamvd <lamvdavid@gmail.com>
2021-01-13 19:25:50 -0500

    Move debugging to TowerBlock class

--- src/main/java/com/dlam/pptowers/blocks/ArrowTowerBlock.java
+++ src/main/java/com/dlam/pptowers/blocks/ArrowTowerBlock.java
@@ -1,32 +1,12 @@
 package com.dlam.pptowers.blocks;
 
 import com.dlam.pptowers.entities.ArrowTowerBlockEntity;
-import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.text.LiteralText;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.hit.BlockHitResult;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockView;
-import net.minecraft.world.World;
 
 public class ArrowTowerBlock extends TowerBlock {
     public ArrowTowerBlock(Settings settings) {
         super(settings);
-    }
-
-    //Will remove later. Using for debugging
-    @Override
-    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
-        if (!world.isClient) {
-            ArrowTowerBlockEntity arrTowEnt = (ArrowTowerBlockEntity) world.getBlockEntity(pos);
-            player.sendMessage(new LiteralText("Target: " + arrTowEnt.getHostileEntities()), false);
-            player.sendMessage(new LiteralText("Target: " + arrTowEnt.target), false);
-        }
-
-        return ActionResult.SUCCESS;
     }
 
     @Override
