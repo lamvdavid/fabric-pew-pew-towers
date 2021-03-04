2d0a875d0751e2974aa2c8a74708bc778f92f886
lamvd <lamvdavid@gmail.com>
2021-03-04 15:34:41 -0500

    Remove redundant fire rate check Change tower to select/attack random nearby hostiles

--- src/main/java/com/dlam/pptowers/entities/LightningTowerBlockEntity.java
+++ src/main/java/com/dlam/pptowers/entities/LightningTowerBlockEntity.java
@@ -1,12 +1,11 @@
 package com.dlam.pptowers.entities;
 
 import com.dlam.pptowers.registry.BlockEntities;
-import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LightningEntity;
-import net.minecraft.entity.projectile.ProjectileEntity;
-import net.minecraft.util.math.Position;
-import net.minecraft.world.World;
+
+import java.util.List;
 
 public class LightningTowerBlockEntity extends InstantTowerBlockEntity {
     //Hard coded stats. May change later if doing tower upgrades
@@ -14,6 +13,9 @@ public class LightningTowerBlockEntity extends InstantTowerBlockEntity {
     public static final double Y_RANGE = 8.0;
     public static final double Z_RANGE = 10.0;
     public static final int FIRE_RATE = 20;
+    public static final int NUM_OF_ATTACKS = 5;
+
+    public List<Entity> hostiles;
 
     public LightningTowerBlockEntity() {
         super(BlockEntities.LIGHTNING_TOWER_BLOCK_ENTITY, X_RANGE, Y_RANGE, Z_RANGE, FIRE_RATE);
@@ -21,14 +23,12 @@ public class LightningTowerBlockEntity extends InstantTowerBlockEntity {
 
     @Override
     protected void shoot() {
-        if(fireRateCounter == fireRate) {
+        hostiles = getHostileEntities();
+        for (int i = 0; i < NUM_OF_ATTACKS; i++) {
+            int targetIndex = (int) (Math.random() * hostiles.size());
             LightningEntity lightning = (LightningEntity) EntityType.LIGHTNING_BOLT.create(this.world);
-            lightning.refreshPositionAfterTeleport(target.getPos());
+            lightning.refreshPositionAfterTeleport(hostiles.get(targetIndex).getPos());
             this.world.spawnEntity(lightning);
-            fireRateCounter = 0;
-        } else {
-            fireRateCounter++;
         }
     }
-
 }
