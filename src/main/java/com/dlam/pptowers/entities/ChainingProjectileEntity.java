cc8fd26c8bcbcf9b53c002386ad3189898f16de5
lamvd <lamvdavid@gmail.com>
2021-03-21 23:03:18 -0400

    Fix not targeting all Hostile mobs Fix not hitting smaller mobs (spiders, silverfish)
    Changed from HostileEntities to MobEntities
    Lowered Y-trajectory of the arrow

--- src/main/java/com/dlam/pptowers/entities/ChainingProjectileEntity.java
+++ src/main/java/com/dlam/pptowers/entities/ChainingProjectileEntity.java
@@ -2,10 +2,14 @@ package com.dlam.pptowers.entities;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.projectile.ArrowEntity;
 import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.RaycastContext;
 import net.minecraft.world.World;
 
 import java.util.List;
@@ -13,7 +17,7 @@ import java.util.stream.Collectors;
 
 /*
     Custom projectile that creates another projectile after hitting an entity
-    Based on
+    Uses ArrowEntity as base
  */
 public class ChainingProjectileEntity extends ArrowEntity {
     //Hard coded stats. May change later if doing tower upgrades
@@ -35,7 +39,7 @@ public class ChainingProjectileEntity extends ArrowEntity {
         super.onEntityHit(entityHitResult);
         range = new Box(this.getPos().getX() - X_RANGE, this.getPos().getY() - Y_RANGE, this.getPos().getZ() - Z_RANGE, this.getPos().getX() + X_RANGE, this.getPos().getY() + Y_RANGE, this.getPos().getZ() + Z_RANGE);
         Entity sourceEntity = entityHitResult.getEntity();
-        HostileEntity target = getClosestHostileEntity(sourceEntity);
+        MobEntity target = getClosestHostileEntity(sourceEntity);
         if(target != null && numOfChains > 0) {
 
             ChainingProjectileEntity chainedArrow = new ChainingProjectileEntity(world, sourceEntity.getX() + targetX, sourceEntity.getEyeY() + targetY, sourceEntity.getZ() + targetZ, numOfChains - 1);
@@ -43,7 +47,7 @@ public class ChainingProjectileEntity extends ArrowEntity {
             double y = target.getBodyY(0.33333333D) - chainedArrow.getY();
             double z = target.getZ() - chainedArrow.getZ();
             double d = (double) MathHelper.sqrt(x * x + z * z);
-            chainedArrow.setVelocity(x, y + d * 0.20000000298023224D, z, 1.6F, 0);
+            chainedArrow.setVelocity(x, y + d * 0.1D, z, 1.6F, 0);
             world.spawnEntity(chainedArrow);
         }
 
@@ -87,26 +91,28 @@ public class ChainingProjectileEntity extends ArrowEntity {
     }
 
     //Returns list of HostileEntities in range
-    public List<HostileEntity> getHostileEntities() {
-        return this.getEntityWorld().getEntitiesByClass(HostileEntity.class,range,null);
+    public List<MobEntity> getHostileEntities() {
+        List<Entity> hostiles = this.getEntityWorld().getEntitiesByClass(HostileEntity.class,range,null);
+        hostiles.addAll(this.getEntityWorld().getEntitiesByClass(MobEntity.class,range,null));
+        return this.getEntityWorld().getEntitiesByClass(MobEntity.class,range,null);
     }
 
     //Gets the closest hostile mob in sight that isn't the source entity from the position from the source entity
-    public HostileEntity getClosestHostileEntity(Entity sourceEntity) {
-        List<HostileEntity> hostileEntities = getHostileEntities();
+    public MobEntity getClosestHostileEntity(Entity sourceEntity) {
+        List<MobEntity> hostileEntities = getHostileEntities();
         hostileEntities.remove(sourceEntity);
         hostileEntities = hostileEntities.stream().filter(e -> e.hurtTime == 0).collect(Collectors.toList());
 
-        HostileEntity target = null;
+        MobEntity target = null;
         double targetDistance = 10000.0;
         double testDistance;
         double[] offset;
 
-        for(HostileEntity e : hostileEntities) {
+        for(MobEntity e : hostileEntities) {
             testDistance = e.squaredDistanceTo(sourceEntity.getX(), sourceEntity.getY(), sourceEntity.getZ());
             if(testDistance < targetDistance) {
                 offset = getTargetDirection(sourceEntity, e);
-                if(e.canSee(sourceEntity)) {
+                if(checkSightLine(sourceEntity,offset[0], offset[1], offset[2])) {
                     setTargetDirection(offset[0], offset[1], offset[2]);
                     target = e;
                     targetDistance = testDistance;
@@ -115,5 +121,13 @@ public class ChainingProjectileEntity extends ArrowEntity {
         }
 
         return target;
+    }
+
+    //Checks if the tower block can see the mob
+    public boolean checkSightLine(Entity entity, double x, double y, double z) {
+        Vec3d vec3d = new Vec3d(this.getX() + x, this.getY() + y, this.getZ() + z);
+        Vec3d vec3d2 = new Vec3d(entity.getX(), entity.getEyeY(), entity.getZ());
+        HitResult.Type test = this.world.raycast(new RaycastContext(vec3d2, vec3d, RaycastContext.ShapeType.COLLIDER, RaycastContext.FluidHandling.NONE, entity)).getType();
+        return test == HitResult.Type.MISS;
     }
 }
