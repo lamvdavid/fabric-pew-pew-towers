1a0b606ac5af825c8f138124a30932161a18ec76
lamvd <lamvdavid@gmail.com>
2021-03-21 23:03:37 -0400

    Remove unused imports

--- src/main/java/com/dlam/pptowers/blocks/ChainingTowerBlock.java
+++ src/main/java/com/dlam/pptowers/blocks/ChainingTowerBlock.java
@@ -1,12 +1,8 @@
 package com.dlam.pptowers.blocks;
 
 import com.dlam.pptowers.entities.ChainingTowerBlockEntity;
-import com.dlam.pptowers.entities.TrajectoryTowerBlockEntity;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.entity.projectile.ProjectileEntity;
-import net.minecraft.util.math.Position;
 import net.minecraft.world.BlockView;
-import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
 public class ChainingTowerBlock extends TowerBlock{
