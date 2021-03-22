0a767711eaecdf0a4379aeb2754aba9cfafffc38
lamvd <lamvdavid@gmail.com>
2021-03-21 23:06:22 -0400

    Comment out debug info

--- src/main/java/com/dlam/pptowers/blocks/TowerBlock.java
+++ src/main/java/com/dlam/pptowers/blocks/TowerBlock.java
@@ -1,21 +1,14 @@
 package com.dlam.pptowers.blocks;
 
-import com.dlam.pptowers.entities.ArrowTowerBlockEntity;
-import net.minecraft.block.*;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.text.LiteralText;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.hit.BlockHitResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockEntityProvider;
 
 public abstract class TowerBlock extends Block implements BlockEntityProvider {
     public TowerBlock(Settings settings) {
         super(settings);
     }
 
-    //Will remove later. Using for debugging
+    /*Will remove later. Using for debugging
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
         if (!world.isClient) {
@@ -26,4 +19,6 @@ public abstract class TowerBlock extends Block implements BlockEntityProvider {
 
         return ActionResult.SUCCESS;
     }
+
+     */
 }
